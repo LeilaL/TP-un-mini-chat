adfc9eb95cb223365daa845fadc5d0e19a11686e
Leila.L <leila.lababsa@hotmail.fr>
2017-09-13 15:57:51 +0200

    create form

--- minichat.php
+++ minichat.php
@@ -19,7 +19,18 @@
   <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
 
-  
+  <form action="cible.php" method="post">
+
+  <p>
+
+      <input type="text" name="pseudo" />
+      <input type="text" name="message" />
+      <input type="submit" value="envoyer" />
+
+  </p>
+
+  </form>
+
 
   <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
