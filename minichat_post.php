813fb0857a6cb1169ae680466d385cd0442965fa
Leila.L <leila.lababsa@hotmail.fr>
2017-09-13 23:21:27 +0200

    added parameter for activate errors

--- minichat_post.php
+++ minichat_post.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=TP mini chat;charset=utf8', 'root', 'leilalababsa');
+    $bdd = new PDO('mysql:host=localhost;dbname=TP mini chat;charset=utf8', 'root', 'leilalababsa', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 
 catch(Exception $e)
@@ -10,7 +10,9 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
+$req = $bdd->prepare('SELECT PSEUDO, MESSAGE FROM minichat WHERE PSEUDO = ? AND MESSAGE = ?');
 
+$req->execute(array($_POST['PSEUDO'], $_POST['MESSAGE']));
 
 
 header('Location: minichat.php');
